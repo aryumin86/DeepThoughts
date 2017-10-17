fcf1627ef1ff4ff9e6c7736bc50238a11a490faa
Alexey Ryumin <aryumin@aryumin.ru>
2017-10-17 19:02:06 +0300

    upd

---
+++ DeepThoughtsLib/DeepThoughtsLib/Entities/Phrase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepThoughtsLib.Entities
+{
+    /// <summary>
+    /// Phrase of somebody.
+    /// </summary>
+    public class Phrase
+    {
+    }
+}
---
+++ DeepThoughtsLib/DeepThoughtsLib/Entities/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepThoughtsLib.Entities
+{
+    /// <summary>
+    /// Collocutor profile - e.g. Pholosopher, Musician etc.
+    /// May be a profession.
+    /// </summary>
+    public class Profile
+    {
+    }
+}
